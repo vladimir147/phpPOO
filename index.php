a68fd490c0bf49389b0cf21f919b038e49d5b2cd
vladimir147 <lenzyko@gmail.com>
2016-12-21 22:17:37 -0500

    estructura del index
    poniendo los la estructura de la pagina principal

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    
+    <title>titulo del proyecto</title>
+</head>
+<body>
+   <h1>etiqueta h1 para el título y buscador de google </h1>
+   
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima consectetur velit at, optio corporis mollitia deleniti harum, iure sapiente dignissimos? Quos, adipisci, saepe! Doloribus ad, iste velit ab, corporis nobis?</p>
+    
+</body>
+</html>
